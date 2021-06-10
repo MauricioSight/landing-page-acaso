06b2f9173b30761b3b82896cca7be3109b63316d
Mauricio Sightman <mauriciosightman@gmail.com>
2021-06-10 01:33:46 -0300

    feature: Ant Design install

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Button } from 'antd';
 
 import GlobalStyle from './styles/global';
+import 'antd/dist/antd.css';
 
 const App: React.FC = () => (
   <>
     <GlobalStyle />
     <div>
       <h1>Landing Page aca.su</h1>
+      <Button type="primary">Button</Button>
     </div>
   </>
 );
